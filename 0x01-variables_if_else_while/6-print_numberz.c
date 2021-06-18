857620e37c40161e08eff1b4ffefc2167cd78b57
mrgiulls <giuliano.mesias@hotmail.com>
2021-06-18 23:58:44 +0000

    Code print all number to 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        only using putchar and without char variables.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-printf("0123456789\n");
+int num;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+putchar('\n');
 return (0);
 }
