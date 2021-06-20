564371b900b05b619e57c2ae09fd0d3c19061cae
mrgiulls <giuliano.mesias@hotmail.com>
2021-06-20 23:36:26 +0000

    Code print digits 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main- Print all single digit numbers
+*Return:Always 0.
+*/
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+printf("%d", num);
+printf("\n");
+return (0);
+}
