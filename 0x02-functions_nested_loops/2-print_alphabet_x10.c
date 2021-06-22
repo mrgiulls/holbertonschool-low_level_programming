a62500b13b1c0672e307e241cba245f0b93ada18
mrgiulls <giuliano.mesias@hotmail.com>
2021-06-22 22:59:55 +0000

    Print the alphabet with putchar ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* print_alphabet_x10 - entry point
+*
+* Description: Prints the alphabet with _putchar and also line
+*
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+char j;
+char c;
+for (j = 0; j < 10; j++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n' );
+}
+}
