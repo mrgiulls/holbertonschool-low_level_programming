f2c32830bfe83308f6703f5012f6ef32bd0b0a3c
mrgiulls <giuliano.mesias@hotmail.com>
2021-06-22 22:24:33 +0000

    Code print Holberton with _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+*main- Code print text Holberton
+*Return: 0.
+
+*/
+int main(void)
+{
+int i = 0;
+char c;
+char s[] = "Holberton\n";
+while (i <= 9)
+{
+c = s[i];
+_putchar(c);
+i++;
+}
+return (0);
+}
