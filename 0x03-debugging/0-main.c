1e8957e1597e3e6e26e502c520b883838e6dc838
mrgiulls <giuliano.mesias@hotmail.com>
2021-06-23 23:52:41 +0000

    Test function that prints is integer is positive o negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *main - test function that prints if integer i positive or neggative
+ *return : porsitive or negative
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
