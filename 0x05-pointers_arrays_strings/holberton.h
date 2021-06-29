4cf087b2f06437354a857dff27a7b91699fe8267
mrgiulls <giuliano.mesias@hotmail.com>
2021-06-29 16:25:41 -0700

    Function that print every other character of string

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -8,3 +8,17 @@ void swap_int(int *a, int *b);
 
 /* prints the length of a string */
 int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
