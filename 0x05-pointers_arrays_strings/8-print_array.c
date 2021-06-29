a74c2a0d1826dd23a75fab1faf7ae99b473ab508
mrgiulls <giuliano.mesias@hotmail.com>
2021-06-29 16:30:31 -0700

    Function thar print n elements of an arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_array - print out array with commas.
+ * @a: is the array of ints;
+ * @n: is the number of elements in array.
+ * Return: void.
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; ++i)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+printf(", ");
+}
+printf("\n");
+}
