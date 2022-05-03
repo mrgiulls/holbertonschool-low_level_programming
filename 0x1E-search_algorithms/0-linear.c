44a94b071db28af4cb34b28cea8252bdb2108b94
mrgiulls <giuliano.mesias@hotmail.com>
2022-05-03 13:35:42 -0500

    update betty style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -6,22 +6,25 @@
  * linear_search - searches for a value in an array
  * @array: is a pointer to the first element
  * @size: is the number of elements in array
- * @objetivo: is the value to search 
+ * @objetivo: is the value to search
  * Return: return value or -1
  */
 
 
-int linear_search(int *array, size_t size, int objetivo){
-    int i;
-    if(array == NULL){
-        return (-1);
-    }
-    for (i = 0; i < (int)size; i++){
-        printf("Value checked array[%d] = [%d]\n", i,array[i]);
-        if (array[i] == objetivo)
-        {
-            return (i);
-        }
-    }
-    return (-1);
+int linear_search(int *array, size_t size, int objetivo)
+{
+int i;
+if (array == NULL)
+{
+return (-1);
+}
+for (i = 0; i < (int)size; i++)
+{
+printf("Value checked array[%d] = [%d]\n", i, array[i]);
+if (array[i] == objetivo)
+{
+return (i);
+}
+}
+return (-1);
 }
